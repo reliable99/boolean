f82aad6a876ace6f8a5959e629c1fcda354856be
reliable99 <abdulmuhminsodiq@gmail.com>
2023-10-12 11:13:49 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("booleans");
+
+//booleans represent a logical entity and can only have two values: true or false
+
+//true - yes -correct, 1
+//false - no, incorrect, 0
+
+const isCool = true;
+
+//lets do something cool with booleans by creating if statement
+
+if (isCool) {
+  console.log("hey man you are cool");
+} else {
+  console.log("you are nice ");
+}
+
+const isNice = false;
+
+if (isNice) {
+  console.log("you are nice ");
+} else {
+  console.log("you are gentle");
+}
+
+// boolean value also comes as a reserve  of comparision
+
+const age = 20;
+
+console.log(age == 21);
